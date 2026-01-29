f87da22a7574278150af5d30e07c5846b66109bf
Duque1001 <anderson.duque@savios.com.co>
2026-01-29 11:02:39 -0500

    Azure Functions Get Mee

--- beneflex-app/src/environments/environment.prod.ts
+++ beneflex-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.tu-dominio.com/api/v1',
+  apiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-user-benefits',
   benefitsApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-user-benefits',
 };
--- beneflex-app/src/environments/environment.ts
+++ beneflex-app/src/environments/environment.ts
@@ -5,8 +5,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/v1',
-  benefitsApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-user-benefits',
+  //apiUrl: 'http://localhost:3000/api/v1',
+  //benefitsApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-user-benefits',
+  apiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-user-benefits',
   msal: {
     clientId: 'c2118f1c-4ac4-4251-84de-10a4274ee244',
     tenantId: '7f32fcbe-7a03-45a9-a3d0-2beb0fa0aaf7'
