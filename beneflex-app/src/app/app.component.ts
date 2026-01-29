ed9b71d6760bd8668ff7d73ef80740163bc17fa0
Duque1001 <anderson.duque@savios.com.co>
2026-01-29 11:38:21 -0500

    Return

--- beneflex-app/src/app/app.component.ts
+++ beneflex-app/src/app/app.component.ts
@@ -10,86 +10,9 @@ export class AppComponent {
   title = 'beneflex-app';
 }*/ // wilson
 
-/*import { Component, OnInit, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MsalService } from '@azure/msal-angular';
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent implements OnInit {
-  private msal = inject(MsalService);
-
-  async ngOnInit() {
-    await this.msal.instance.initialize();
-    const result = await this.msal.instance.handleRedirectPromise();
-
-    if (result?.account) {
-      this.msal.instance.setActiveAccount(result.account);
-    } else {
-      const accounts = this.msal.instance.getAllAccounts();
-      if (accounts.length) this.msal.instance.setActiveAccount(accounts[0]);
-    }
-  }
-}*/ //Duque
-
-/*import { Component, OnInit, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { MsalService } from '@azure/msal-angular';
-import { UserService } from './core/services/user.service'; // ajusta ruta
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent implements OnInit {
-  private msal = inject(MsalService);
-  private http = inject(HttpClient);
-  private userService = inject(UserService);
-
-  async ngOnInit() {
-    // MSAL v3
-    await this.msal.instance.initialize();
-
-    // Si venimos de loginRedirect
-    const result = await this.msal.instance.handleRedirectPromise();
-
-    // Set active account
-    if (result?.account) {
-      this.msal.instance.setActiveAccount(result.account);
-    } else {
-      const accounts = this.msal.instance.getAllAccounts();
-      if (accounts.length) this.msal.instance.setActiveAccount(accounts[0]);
-    }
-
-    this.http
-      .get('https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-me')
-      .subscribe({
-        next: (user: any) => {
-          this.userService.setUser(user);
-          console.log('Usuario cargado:', user);
-        },
-        error: (err) => {
-          console.error('Error cargando usuario (/get-me):', err);
-        }
-      });
-  }
-}*/
-
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MsalService } from '@azure/msal-angular';
-import { HttpClient } from '@angular/common/http';
-import { UserService } from './core/services/user.service';
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -100,11 +23,8 @@ import { environment } from '../environments/environment';
 })
 export class AppComponent implements OnInit {
   private msal = inject(MsalService);
-  private http = inject(HttpClient);
-  private userService = inject(UserService);
 
   async ngOnInit() {
-    // 1) Inicializa MSAL y procesa redirect
     await this.msal.instance.initialize();
     const result = await this.msal.instance.handleRedirectPromise();
 
@@ -114,18 +34,5 @@ export class AppComponent implements OnInit {
       const accounts = this.msal.instance.getAllAccounts();
       if (accounts.length) this.msal.instance.setActiveAccount(accounts[0]);
     }
-
-    // 2) Ya con sesión: intenta cargar el "me"
-    this.http.get(`${environment.apiUrl}/api/get-me`)
-      .subscribe({
-        next: (user: any) => {
-          this.userService.setUser(user);
-          console.log('Usuario cargado:', user);
-        },
-        error: (err) => {
-          console.error('Error cargando usuario (/get-me):', err);
-        }
-      });
   }
 }
-
--- beneflex-app/src/environments/environment.prod.ts
+++ beneflex-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net',
-  benefitsApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net',
+  apiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-user-benefits',
+  benefitsApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-user-benefits',
 };
