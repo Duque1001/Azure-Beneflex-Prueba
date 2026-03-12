65bd97eea67106f402ae5ae75983b61f15f59bde
Duque1001 <anderson.duque@savios.com.co>
2026-03-12 17:10:59 -0500

    Actualizacion variables 2

--- beneflex-app/src/environments/environment.prod.ts
+++ beneflex-app/src/environments/environment.prod.ts
@@ -9,4 +9,9 @@ export const environment = {
   approvalsApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-my-requests',
   pendingRequestsApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-pending-requests',
   updateRequestStatusApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/update-request-status',
+
+  msal: {
+    clientId: 'e29998d4-12f7-4400-a69d-2b3268db9650',
+    tenantId: 'd3852f48-3c9a-48c2-afc9-1d4bf09fb882'
+  }
 };
