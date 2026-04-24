b4cf91e6544f32752a142df4391d993a79d7e4ca
Duque1001 <anderson.duque@savios.com.co>
2026-04-24 09:39:10 -0500

    Actualizacion de URL azure functions

--- beneflex-app/src/environments/environment.ts
+++ beneflex-app/src/environments/environment.ts
@@ -2,14 +2,14 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:3000/api/v1',
-  functionsApiBaseUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api',
-  meApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-me',
-  benefitsApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-user-benefits',
-  createBenefitApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/create-benefit-request',
-  myRequestsApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-my-requests',
-  approvalsApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-my-requests',
-  pendingRequestsApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/get-pending-requests',
-  updateRequestStatusApiUrl: 'https://beneflex-functions-ajfsbxbfetexc9cu.canadacentral-01.azurewebsites.net/api/update-request-status',
+  functionsApiBaseUrl: 'https://funciones-beneflex-c5a7hhfpe6gjcxbj.canadacentral-01.azurewebsites.net/api',
+  meApiUrl: 'https://funciones-beneflex-c5a7hhfpe6gjcxbj.canadacentral-01.azurewebsites.net/api/get-me',
+  benefitsApiUrl: 'https://funciones-beneflex-c5a7hhfpe6gjcxbj.canadacentral-01.azurewebsites.net/api/get-user-benefits',
+  createBenefitApiUrl: 'https://funciones-beneflex-c5a7hhfpe6gjcxbj.canadacentral-01.azurewebsites.net/api/create-benefit-request',
+  myRequestsApiUrl: 'https://funciones-beneflex-c5a7hhfpe6gjcxbj.canadacentral-01.azurewebsites.net/api/get-my-requests',
+  approvalsApiUrl: 'https://funciones-beneflex-c5a7hhfpe6gjcxbj.canadacentral-01.azurewebsites.net/api/get-my-requests',
+  pendingRequestsApiUrl: 'https://funciones-beneflex-c5a7hhfpe6gjcxbj.canadacentral-01.azurewebsites.net/api/get-pending-requests',
+  updateRequestStatusApiUrl: 'https://funciones-beneflex-c5a7hhfpe6gjcxbj.canadacentral-01.azurewebsites.net/api/update-request-status',
 
   msal: {
     // clientId: '7d035349-8064-4ad8-82a6-631255cde4a4',
